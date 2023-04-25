957b88f051ba95cde4fd40ea112d6fb8def2dfbf
Ana <anapaula@ana.mat.br>
2023-04-25 18:31:39 -0300

    versao nova

---
+++ Exemplo1.R
@@ -0,0 +1,22 @@
+# criando o vetor idades
+idades <- c( 23, 18, 17, 25, 21, 19, 22, 24, 19, 19 )
+
+# maior valor
+# função max( )
+max(idades)
+
+# menor valor
+# função min( )
+min(idades)
+
+# faixa de valores
+# função range( )
+range(idades)
+
+# média (mean)
+# função mean( )
+mean(idades)
+
+# desvio padrão (standard deviation)
+# função sd()
+sd(idades)
